61c72e6cba937314300b6c6f04798df6a24f4e8f
Alexander <stdAlexTikhonov@hotmail.com>
2021-10-29 22:26:40 +0300

    Random vectors

--- src/components/RandomVectors/index.tsx
+++ src/components/RandomVectors/index.tsx
@@ -12,22 +12,26 @@ export const RandomVectors: React.FC<ComponentProps> = (
     //See annotations in JS for more information
 
     const setup = (p5: p5Types, canvasParentRef: Element) => {
-        p5.createCanvas(window.innerWidth / 2, window.innerHeight - 100).parent(
+        p5.createCanvas(400, 400).parent(
             canvasParentRef
         );
-        p5.background(0);
+
     };
 
     const draw = (p5: p5Types) => {
 
-        p5.translate(p5.width / 2, p5.height / 2);
-
+        p5.background(0);
+        let pos = p5.createVector(200, 200);
         // let v = p5.createVector(p5.random(-100, 100), p5.random(-100, 100));
-        let v = p5Types.Vector.random2D();
-        v.mult(p5.random(50, 100));
+        let mouse = p5.createVector(p5.mouseX, p5.mouseY);
 
-        p5.strokeWeight(4);
-        p5.stroke(255, 50);
+        let v = p5Types.Vector.sub(mouse, pos);
+        // v.normalize();
+        // v.mult(50);
+        v.setMag(50);
+        p5.translate(p5.width / 2, p5.height / 2);
+        p5.strokeWeight(2);
+        p5.stroke(255);
         p5.line(0, 0, v.x, v.y);
     };
 
