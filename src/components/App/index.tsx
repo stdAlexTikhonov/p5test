336194889eb33bda3f5756ee71c01f6782578ceb
Alexander <stdAlexTikhonov@hotmail.com>
2021-11-05 00:48:18 +0300

    overflow

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -18,6 +18,7 @@ export default function App() {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        overflow: 'hidden'
       }}
     >
       <MoonMotion />
